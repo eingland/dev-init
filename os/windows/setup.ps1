c365f4a939a2e912ae50765f1f48757fb6680e9d
Eric Ingland <4283616+eingland@users.noreply.github.com>
2022-01-29 18:46:23 -0600

    fix path

--- os/windows/setup.ps1
+++ os/windows/setup.ps1
@@ -22,4 +22,4 @@ Write-Output "Install configs"
 Copy-Item ~\AppData\Local\Programs\oh-my-posh\themes\blueish.omp.json ~
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eingland/dev-init/main/os/windows/config/Microsoft.PowerShell_profile.ps1' -OutFile $PROFILE
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eingland/dev-init/main/os/windows/config/settings.json' -OutFile '~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json'
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eingland/dev-init/main/os/windows/config/.gitconfig' -OutFile ~
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eingland/dev-init/main/os/windows/config/.gitconfig' -OutFile '~\.gitconfig'
